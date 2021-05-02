23a545f48a70230a2c99e0ad6231da45a369faf2
shunhon <shun.honda.development@gmail.com>
2021-05-02 16:18:45 +0900

    add task class

---
+++ src/task.ts
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from "uuid";
+
+export abstract class Task<Input, Output> {
+  private id: string;
+  private _dependencies: Task<unknown, Input>[];
+
+  public constructor() {
+    this.id = uuidv4();
+    this._dependencies = [];
+  }
+
+  public get dependencies() {
+    return this._dependencies;
+  }
+
+  public getTaskName() {
+    return this.constructor.name;
+  }
+
+  public getTaskId() {
+    return this.id;
+  }
+
+  public conditions() {
+    return true;
+  }
+
+  public dependsOn(task: Task<unknown, Input>) {
+    this.dependencies.push(task);
+  }
+
+  public input(): Input[] {
+    return this.dependencies.map((dependency) => {
+      return dependency.output();
+    });
+  }
+
+  public abstract output(): Output;
+
+  public abstract run(): void;
+}
