8b6b69a278fffb71aeb5afbb87778a82347632f5
shunhon <shun.honda.development@gmail.com>
2021-05-05 14:11:56 +0900

    add addInput method for to explicitly add an input

--- src/task.ts
+++ src/task.ts
@@ -3,10 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 export abstract class Task<Input, Output> {
   private _id: string;
   private _dependencies: Task<unknown, Input>[];
+  private _inputs: Input[];
 
   public constructor() {
     this._id = uuidv4();
     this._dependencies = [];
+    this._inputs = [];
   }
 
   public get dependencies(): Task<unknown, Input>[] {
@@ -29,10 +31,16 @@ export abstract class Task<Input, Output> {
     this.dependencies.push(task);
   }
 
+  public addInput(input: Input): void {
+    this._inputs.push(input);
+  }
+
   public input(): Input[] {
-    return this.dependencies.map((dependency) => {
-      return dependency.output();
-    });
+    return this.dependencies
+      .map((dependency) => {
+        return dependency.output();
+      })
+      .concat(this._inputs);
   }
 
   public abstract output(): Output;
